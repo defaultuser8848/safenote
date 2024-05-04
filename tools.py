9f7da8c5aabca08b82f6ec1bc933aaac9cf4931e
defaultuser8848 <158047456+defaultuser8848@users.noreply.github.com>
2024-05-05 00:30:15 +0800

    Create tools.py

---
+++ tools.py
@@ -0,0 +1,8 @@
+import asyncio
+async def getContent(page):
+    #TODO:获取page上的内容并返回，如果无法获取，返回False
+    pass
+async def setContnet(page,content):
+    #TODO:将page上的内容设置为content，返回http状态码（成功为200）
+    pass
+    
